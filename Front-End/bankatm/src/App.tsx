825ac9dde99303def1bef0c50f5826c4d40164a8
Davi Andrzejewski Junkes <108496905+JnksDavu@users.noreply.github.com>
2025-03-09 18:10:00 -0300

    Adicionado conexão com o front-end em react

---
+++ Front-End/bankatm/src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import api from "./services/api";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    api.get("/users").then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Lista de Usuários</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
